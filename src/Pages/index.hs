2b6a9bbcdf1b124a4e20f45b6e8d06bda08ca13f
Hilton Pintor <hiltonpintor@gmail.com>
2018-06-12 20:09:57 -0300

    adding img and style folders

--- src/Pages/index.hs
+++ src/Pages/index.hs
@@ -8,9 +8,16 @@ import qualified Text.Blaze.Html4.Strict.Attributes as A
 
 import HtmlHelper
 
-helloBlaze :: ServerPart Response
-helloBlaze = 
+index :: ServerPart Response
+index = 
     ok $ toResponse $
-        htmlTemplate "Hello, Blaze!"
+        pageBuilder "Hello, Blaze!"
                     [H.meta ! A.name "keywords" ! A.content "happstack, blaze, html"]
-                    (H.p "This is a <p> tag")
+                    header
+
+
+
+header :: H.Html
+header = H.div ! A.class_ "header" $ do
+            H.p "This is a <p> tag"
+            H.img ! A.class_ "logo" ! A.src "img/haskell-logo.png"
