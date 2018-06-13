1d3c273b1f50e785fa7886b8e38847f8246a95f0
Hilton Pintor <hiltonpintor@gmail.com>
2018-06-13 10:19:47 -0300

    Adding content with mock text

--- src/htmlHelper.hs
+++ src/htmlHelper.hs
@@ -8,6 +8,7 @@ import qualified Text.Blaze.Html4.Strict.Attributes as A
 import Control.Monad (msum)
 
 
+-- Building block for pages --
 pageBuilder :: String -> [H.Html] -> H.Html -> H.Html
 pageBuilder title headers body =
     H.html $ do
@@ -18,3 +19,25 @@ pageBuilder title headers body =
             sequence_ headers
         H.body $ do
             body
+
+
+-- Turns list text into <p> tags --
+paragraphsToP :: [String] -> [H.Html]
+paragraphsToP paragraphs = map (H.p . H.toHtml) paragraphs
+
+
+-- Turns list text into <ul> --
+itemsToUl :: [String] -> H.Html
+itemsToUl items = H.ul $ do
+                    sequence_ $ map (H.li . H.toHtml) items
+
+
+-- An empty (flex: 1) div used for margin --
+marginDiv :: H.Html
+marginDiv = H.div ! A.class_ "margin" $ do
+                     H.p "" -- TODO: Replace with empty div
+
+
+-- Lorem Ipsum --
+loremBuba :: Int -> String
+loremBuba times = concat $ (replicate times "Belly Buba ")
