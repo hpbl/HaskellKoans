0f78c08e2678eca841d9f011403fba29c7512db2
Hilton Pintor <hiltonpintor@gmail.com>
2018-06-13 09:38:37 -0300

    Dividing header and content components

--- src/Pages/index.hs
+++ src/Pages/index.hs
@@ -8,16 +8,46 @@ import qualified Text.Blaze.Html4.Strict.Attributes as A
 
 import HtmlHelper
 
-index :: ServerPart Response
-index = 
-    ok $ toResponse $
-        pageBuilder "Hello, Blaze!"
-                    [H.meta ! A.name "keywords" ! A.content "happstack, blaze, html"]
-                    header
+-- Header --
+logoDiv :: H.Html
+logoDiv = H.div ! A.class_ "logo" $ do
+              H.img ! A.src "img/haskell-logo.png"
 
+titleDiv :: H.Html
+titleDiv = H.div ! A.class_ "title" $ do
+               H.h1 "Haskell Koans"
 
+rightMarginDiv :: H.Html
+rightMarginDiv = H.div ! A.class_ "right" $ do
+                     H.p "" -- TODO: Replace with empty div
 
 header :: H.Html
 header = H.div ! A.class_ "header" $ do
-            H.p "This is a <p> tag"
-            H.img ! A.class_ "logo" ! A.src "img/haskell-logo.png"
+             logoDiv
+             titleDiv
+             rightMarginDiv
+
+
+-- Content --
+loremBuba :: String
+loremBuba = concat $ (replicate 1000 "Belly Buba ")
+
+content :: H.Html
+content = H.div ! A.class_ "content" $ do
+              H.p (H.toHtml loremBuba)
+
+
+-- Container --
+container :: H.Html
+container = H.div ! A.class_ "container" $ do
+                header
+                content
+
+
+-- Index.html --
+index :: ServerPart Response
+index = 
+    ok $ toResponse $
+        pageBuilder "Haskell Koans"
+                    [H.meta ! A.name "keywords" ! A.content "haskell, koans, programming"]
+                    container
