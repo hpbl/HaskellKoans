0d8ea458dba7aca1413b6f95adc564f5a6b26db3
Hilton Pintor <hiltonpintor@gmail.com>
2018-06-19 09:33:18 -0300

    displaying koan

--- src/Pages/exercise.hs
+++ src/Pages/exercise.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Pages.Koans where
+module Pages.Exercise where
 
 import Happstack.Server
 import Text.Blaze ((!))
 import qualified Text.Blaze.Html4.Strict as H
 import qualified Text.Blaze.Html4.Strict.Attributes as A
+import Data.Text (splitOn)
 
+import Model.Koan (Koan, checkAnswer, codeParts, koans, intro)
 import HtmlHelper (pageBuilder, paragraphsToP, marginDiv, itemsToUl)
+
 
 -- Top --
 top :: H.Html
@@ -21,12 +24,20 @@ navigationDiv = H.div ! A.class_ "navigation" $ do
                     H.input ! A.type_ "submit" ! A.class_ "navButton" ! A.value "<"
                     H.input ! A.type_ "submit" ! A.class_ "navButton" ! A.value ">"
 
-middle :: H.Html
-middle = H.div ! A.class_ "middle" $ do
-             H.form ! A.enctype "multipart/form-data" ! A.action "/input" ! A.method "POST" $ do
-                 H.h2 "Make it True"
-                 H.input ! A.type_ "text" ! A.value "True /= " ! A.name "answer"
-                 navigationDiv
+koanToHtml :: Koan -> [H.Html]
+koanToHtml koan = prefix : (H.input ! A.type_ "text") : sufix : []
+    where parts  = codeParts koan
+          prefix = H.p (H.toHtml $ fst parts)
+          sufix  = H.p (H.toHtml $ snd parts)
+
+middle :: Koan -> H.Html
+middle koan = H.div ! A.class_ "middle" $ do
+                  H.form ! A.enctype "multipart/form-data" ! A.action "/input" ! A.method "POST" $ do
+                      H.h2 $ H.toHtml $ intro koan -- the koan intro text
+                      H.div ! A.class_ "koan" $ do
+                        sequence_ $ koanToHtml koan
+                      --H.input ! A.type_ "text" ! A.value "True /= " ! A.name "answer"
+                      navigationDiv
                  
 
 -- Bottom --
@@ -39,17 +50,17 @@ bottom = H.div ! A.class_ "bottom" $ do
 container :: H.Html
 container = H.div ! A.class_ "container" $ do
                 top
-                middle
+                middle $ head koans
                 bottom
 
 
 -- Koans.html --
-koans :: ServerPart Response
-koans = 
+exercise :: ServerPart Response
+exercise = 
     ok $ toResponse $
         pageBuilder "Haskell Koans"
                     [
                         H.meta ! A.name "keywords" ! A.content "haskell, koans, programming",
-                        H.link ! A.rel "stylesheet" ! A.type_ "text/css" ! A.href "style/koans.css"
+                        H.link ! A.rel "stylesheet" ! A.type_ "text/css" ! A.href "style/exercise.css"
                     ]
                     container
