f9f384d8d9cd160daee060de19048b1345045e6d
Hilton Pintor <hiltonpintor@gmail.com>
2018-06-24 18:38:10 -0300

    navigating between koan themes

--- src/helpers/routingHelper.hs
+++ src/helpers/routingHelper.hs
@@ -1,6 +1,7 @@
 module Helpers.RoutingHelper
  (
-   redirect
+   redirect,
+   splitOnKeyword
 ) where
 
 import Happstack.Server hiding (redirect)
@@ -8,3 +9,10 @@ import Happstack.Server hiding (redirect)
 -- Redirect to another URL --
 redirect :: String -> ServerPart Response
 redirect url = seeOther url (toResponse "")
+
+-- splits a string into two parts (before, after) keyword
+splitOnKeyword :: String -> String -> Char -> (String, String)
+splitOnKeyword [] _ _               = error "couldn't find keyword on string"
+splitOnKeyword (x:xs) accum keyword = if x == keyword
+                                        then (accum, xs)
+                                        else splitOnKeyword xs (accum ++ [x]) keyword
