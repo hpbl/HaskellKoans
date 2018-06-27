8a69d2d97a59e482b67689a7b94a91905587394d
Isabelly <icfd@cin.ufpe.br>
2018-06-26 23:41:59 -0300

    remove main function

--- http-request.hs
+++ http-request.hs
@@ -1,9 +1,9 @@
--- Examples of handling for JSON responses
---
--- This library provides several ways to handle JSON responses
-
 {-# LANGUAGE DeriveGeneric, OverloadedStrings, ScopedTypeVariables #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
+
+module HTTPRequest()
+where
+
 
 import Control.Lens ((&), (^.), (^?), (.~))
 import Data.ByteString.Lazy.Internal (ByteString(..))
@@ -18,25 +18,21 @@ import qualified Data.Vector as V
 
 import Network.Wreq
 
+data Koan = Koan {
+  id :: Int,
+  sentence :: String,
+  theme :: String,
+  answer :: String
+  } deriving (Show)
 
-newtype KoanList = KoanList [Koan] deriving (Show)
-
-data Koan = Koan {id :: Int, sentence :: String } deriving (Show)
-
--- instance FromJSON KoanList where
---   parseJSON (Object o) = do
---     (do rawData <- o .: "rawData" KoanList . V.toList <$> mapM parseJSON rawData)
---   parseJSON err = fail $ "Invalid type for KoanList: " ++ show err
---
--- instance FromJSON Koan where
---   parseJSON (Object o) = Koan <$> o .: "id" <*> o .: "sentence"
---   parseJSON err =  fail $ "Invalid type for Koan: " ++ show err
 
 parseKoan :: Value -> Parser Koan
 parseKoan = withObject "object" $ \o -> do
     id <- o .: "id"
     sentence <- o .: "sentence"
-    return $ Koan id sentence
+    theme <- o .: "theme"
+    answer <- o .: "answer"
+    return $ Koan id sentence theme answer
 
 parseKoanList' :: Value -> Parser [Koan]
 parseKoanList' = withArray "array" $ \arr ->
@@ -47,18 +43,14 @@ parseKoanList = withObject "object" $ \o -> do
     rawData <- o .: "rawData"
     parseKoanList' rawData
 
-printDataFromServer :: IO ()
+printDataFromServer :: Maybe a
 printDataFromServer = do
   response <- (get "https://young-taiga-18731.herokuapp.com/data")
   let strJson = (case (response ^? responseBody) of
                    Just value -> value
                    Nothing -> Empty)
+  let parseResult = (case decode strJson of
+                  Just result -> Just (parse parseKoanList result)
+                  Nothing -> Nothing)
 
-  case decode strJson of
-    Just result -> print $ parse parseKoanList result
-    Nothing -> fail "decoding failed"
-
-
-main :: IO ()
-main = do
-  printDataFromServer
+  return parseResult
