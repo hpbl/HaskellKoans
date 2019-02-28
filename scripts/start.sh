0616ca4fb9a0b05b58232991c58a953a84b67795
Hilton Pintor <hiltonpintor@gmail.com>
2019-02-28 11:35:14 -0300

    Detaching on docker run

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,5 +9,5 @@ then
 fi
 
 # Mapping port 8080 from local machine to port $PORT of container and passing $PORT as an environment variable
-docker run -e PORT=$PORT --publish 8080:$PORT --name happstack-server happstack-server
+docker run -e PORT=$PORT --publish 8080:$PORT --detach --name happstack-server happstack-server
 
